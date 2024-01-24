ced428004f9f0b28edd88971a1976a1d5e8a692d
liujuan0616 <liujuan0616@gmail.com>
2024-01-23 21:30:14 -0500

    Magic 8 Ball express application created.

--- software_homework/unit_3/week12_day2_lab/server.js
+++ software_homework/unit_3/week12_day2_lab/server.js
@@ -17,9 +17,35 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
 
 })
 
+const resArray = ["It is certain", 
+"It is decidedly so", 
+"Without a doubt", 
+"Yes definitely",
+"You may rely on it", 
+"As I see it yes", 
+"Most likely", 
+"Outlook good",
+"Yes", 
+"Signs point to yes", 
+"Reply hazy try again", 
+"Ask again later",
+"Better not tell you now", 
+"Cannot predict now",
+ "Concentrate and ask again",
+ "Don't count on it",
+  "My reply is no",
+   "My sources say no",
+   "Outlook not so good",
+    "Very doubtful"]
+
+    app.get('/magic/:question', (req, res) => {
+        const randomNum = Math.floor(Math.random()*resArray.length)
+        res.send(`<h1>${req.params.question}?</h1> <h1>${resArray[randomNum]}</h1>`)
+    })
 
 
 
-app.listen(3001, () => {
-    console.log('listening on port 3001')
+
+app.listen(3002, () => {
+    console.log('listening on port 3002')
   })
