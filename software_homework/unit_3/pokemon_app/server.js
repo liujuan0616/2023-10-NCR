ff8321efe14dec7e459f14aedbcc598e5793c7ef
liujuan0616 <liujuan0616@gmail.com>
2024-01-28 17:58:09 -0500

    Show view shows req.params.id

--- software_homework/unit_3/pokemon_app/server.js
+++ software_homework/unit_3/pokemon_app/server.js
@@ -15,6 +15,10 @@ app.get('/pokemon', function(req, res){
     res.render('Index', {pokemon: pokemon})
 })
 
+app.get('/pokemon/:id', (req, res)=>{
+    res.send(req.params.id)
+})
+
 
 
 app.listen(port, () => {
